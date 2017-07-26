359cac52a4fc1ddb597f9ebafd925190e9d78e48
Yonghwan SO <scinix@gmail.com>
2017-07-26 23:40:45 +0900

    initial version of i18n translations

---
+++ scripts/extract_locale.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# vim: set ts=2 sw=2 expandtab:
+
+langs="en-us ko-kr"
+
+for lang in $langs; do
+  echo -e "\n\nLANG: $lang"
+
+  find templates -type f |xargs cat \
+  |sed 's/t("/\nt("/g' \
+  |grep 't("' \
+  |sed 's/.*t("\([\._a-z0-9]*\)").*/- id: \1Xtranslation: "\1"/' \
+  |while read line; do
+    id=`echo $line|cut -dX -f1`
+    grep -q "^$id\$" locales/*.$lang.yaml || echo "$line"
+  done | sort -u | sed 's/X/\n/'
+
+  grep '"\w*\(\.\w*\)*\.\w*"' `find actions -type f` \
+  |grep -v "\.html" \
+  |sed 's/.*"\(\w*\(\.\w*\)*\.\w*\)".*/- id: \1Xtranslation: "\1"/' \
+  |while read line; do
+    id=`echo $line|cut -dX -f1`
+    grep -q "^$id\$" locales/*.$lang.yaml || echo "$line"
+  done | sort -u | sed 's/X/\n/'
+done
